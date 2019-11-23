2d1ed4a2d208c8b81acfa8c8a072d84d2b81dccc
Roman <r.tchirkov2010@yandex.ru>
2019-11-23 12:34:32 +0300

    Update ShapeAreaClass.cs

--- ShapeAreaLibrary/ShapeAreaClass.cs
+++ ShapeAreaLibrary/ShapeAreaClass.cs
@@ -2,41 +2,46 @@
 
 namespace ShapeAreaLibrary
 {
+    //Родительский класс 
     public abstract class Shape
     {
         protected double area;
 
         public abstract double CalcArea();
     }
-
+    //Класс Круг
     public class Circle : Shape
     {
         private double radius;
-
+        
+        //Конструктор, принимающий значение радиуса
         public Circle(double r)
         {
             radius = r;
         }
-
+        //Метод, вычисляющий площадь круга
         public override double CalcArea()
         {
-            area = Math.PI * (Math.Pow(radius, 2));
+            area = Math.Round(Math.PI,2) * (Math.Pow(radius, 2));
             return (area);
         }
     }
-
+    //Класс Треугольник
     public class Rectangle : Shape
     {
         private double cathetus_a;
         private double cathetus_b;
         private double hypotenuse;
 
+        //Конструктор принимающий значения трех сторон
         public Rectangle(double a, double b, double h)
         {
             cathetus_a = a;
             cathetus_b = b;
             hypotenuse = h;
         }
+
+        //Метод вычисляющий площадь треугольника
         public override double CalcArea()
         {
             double p = (cathetus_a + cathetus_b + hypotenuse) / 2;
